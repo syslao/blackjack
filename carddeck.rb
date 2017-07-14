87807c2f89d1046f1b81d64a81e279edf1aea55f
modems <syslao@gmail.com>
2017-07-15 00:45:37 +0300

    add carddeck class

---
+++ carddeck.rb
@@ -0,0 +1,21 @@
+class CardDeck
+  SUITS = ["\u{2664}", "\u{2661}", "\u{2667}", "\u{2662}"].freeze
+  CARD_VALUES = {
+    '2' => 2, '3'  => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7, '8' => 8,
+    '9' => 9, '10' => 10, 'K' => 10, 'J' => 10, 'Q' => 10, 'A' => 1
+  }.freeze
+
+  attr_accessor :deck
+
+  def initialize
+    @deck = create
+  end
+
+  def create
+    @deck = []
+    SUITS.each do |suits|
+      CARD_VALUES.each { |card, value| @deck << [card + suits, value] }
+    end
+    @deck.shuffle!
+  end
+end
