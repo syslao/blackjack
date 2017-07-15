5a39d3ff000cca2c99245455d54d58978185cf3c
modems <syslao@gmail.com>
2017-07-15 20:56:54 +0300

    add user actions in menu

--- gameinterface.rb
+++ gameinterface.rb
@@ -4,10 +4,20 @@ class GameInterface
     @deck   = CardDeck.new
     @dealer = Dealer.new
     @user   = create_user
+    @actions = ['1-Пропустить ход', '2-Взять карту', '3-Открыть карты', '0-Выход']
   end
 
   def start
     make_bet
+    user_actions
+  end
+
+  def user_actions
+    @actions.each { |action| puts "#{action} " }
+    @user_choice = gets.to_i
+  end
+
+  def execute_action
   end
 
   def make_bet(value = 10)
