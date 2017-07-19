41a2a258301910d3b02f55389259eb7dfd1845aa
syslao <syslao@gmail.com>
2017-07-19 15:48:50 +0300

    Dealer name change

--- dealer.rb
+++ dealer.rb
@@ -1,10 +1,10 @@
 class Dealer < Player
-  def initialize(name = 'NPC')
+  def initialize(name = 'Dealer')
     super
     @name = name
   end
 
   def show_cards_back
-    cards.each { print "X " }
+    cards.each { print 'X ' }
   end
 end
